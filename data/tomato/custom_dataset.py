7a9e7b30c447c968c861dc54c8d29af3e35c8cd2
Andrew Garza <andrewg1232@gmail.com>
2024-03-24 17:05:47 -0700

    updated and fixed pretrained

--- data/tomato/custom_dataset.py
+++ data/tomato/custom_dataset.py
@@ -7,11 +7,12 @@ class CustomDataset(Dataset):
     
     def __len__(self):
         return len(self.data)
+        # return 10
     
     def __getitem__(self, idx):
         image, label = self.data[idx]
 
-        image = torch.tensor(image)
+        image = torch.tensor(image).float() / 255.0
         label = torch.tensor(label)
 
         return (image, label)
